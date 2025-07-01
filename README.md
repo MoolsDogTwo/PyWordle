aa88b5478668f2b4d9e57c25cb808f10634cfb99
Aleksandar Milović <107961418+MoolsDogTwo@users.noreply.github.com>
2025-07-01 10:38:19 +0200

    builded and the game is ready

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # PyWordle
-Just a wordle clone created with PyGame. I did this for fun, and to get better at programming in general.
+Just a small wordle clone created with PyGame. I did this for fun, and to get better at programming in general.
 
-# Warning
-This isn't even functional right now, make no attempt to play okay.
-Why am I even pushing unfinished code on GitHub? Cuz I can lol.
+# How to run
+Go to dist/
--- wordle.py
+++ wordle.py
@@ -17,6 +17,7 @@ COL_ERROR = pygame.Color(255, 0, 0)
 COL_RIGHT_SPOT = pygame.Color(0, 160, 0)
 COL_RIGHT_LETTER = pygame.Color(160, 130, 0)
 COL_EMPTY = pygame.Color(50, 50, 50)
+COL_CURRENT_ROW = pygame.Color(75, 75, 75)
 RECT_SIZE = 50
 RECT_PAD = 5
 words = None
@@ -40,14 +41,24 @@ def input_word(key, current_word) -> str:
 
 
 def is_in_word_list(current_word) -> bool:
-    return current_word.lower() in words
+    return current_word in words
 
 
-def check_word(current_word) -> tuple[bool, str]:
+def check_word(current_word, word, current_attempt, word_slot_data) -> tuple[bool, str]:
+    current_word = current_word.lower()
     if not len(current_word) == MAX_WORD_LENGTH:
         return (False, "Word must be 5 letters long")
     if not is_in_word_list(current_word):
         return (False, "Not in word list")
+    
+    # Check word correctness
+    for i, letter in enumerate(current_word):
+        if current_word[i] == word[i]:
+            word_slot_data[current_attempt][i] = 2
+        elif letter in word:
+            word_slot_data[current_attempt][i] = 1
+        else:
+            word_slot_data[current_attempt][i] = 0
     return (True, "")
 
 
@@ -57,26 +68,38 @@ def t_pad(pos):
     return pos + RECT_PAD
 
 
-def get_box_colour(i, j, word_slots, word):
-    try:
-        if word_slots[i]:
-            current_word = word_slots[i].lower()
-            if current_word[j] in word: # Check if the current letter exists in the word
-                return COL_RIGHT_LETTER
-            # TODO: Check if the current letter is in the right spot
-            else:
-                return COL_EMPTY
-    except IndexError:
-        pass
-    return COL_EMPTY
+def get_box_colour(i, j, word_slot_data, current_attempt):
+    if i == current_attempt:
+        return COL_CURRENT_ROW
+
+    match word_slot_data[i][j]:
+        case 0:
+            return COL_EMPTY
+        case 1:
+            return COL_RIGHT_LETTER
+        case 2:
+            return COL_RIGHT_SPOT
+        case _:
+            raise IndexError("Word slot data should be between 0 and 2.")
+    # try:
+    #     if word_slots[i]:
+    #         current_word = word_slots[i].lower()
+    #         if current_word[j] in word: # Check if the current letter exists in the word
+    #             return COL_RIGHT_LETTER
+    #         # TODO: Check if the current letter is in the right spot
+    #         else:
+    #             return COL_EMPTY
+    # except IndexError:
+    #     pass
+    # return COL_EMPTY
 
 
-def draw_ui(scr, word, word_slots, current_word, current_attempt) -> None:
+def draw_ui(scr, word, word_slots, word_slot_data, current_word, current_attempt) -> None:
     for i in range(SLOT_COUNT):
         for j in range(MAX_WORD_LENGTH):
             left_pos = j * (RECT_SIZE + RECT_PAD)
             top_pos = i * (RECT_SIZE + RECT_PAD)
-            pygame.draw.rect(scr, get_box_colour(i, j, word_slots, word), pygame.Rect(left_pos, top_pos, RECT_SIZE, RECT_SIZE))
+            pygame.draw.rect(scr, get_box_colour(i, j, word_slot_data, current_attempt), pygame.Rect(left_pos, top_pos, RECT_SIZE, RECT_SIZE))
             if i == current_attempt:  # Draw the current word differently to the word slots
                 try:
                     draw_text(scr, current_word[j], (t_pad(left_pos), t_pad(top_pos)), WORD_FONT, COL_WHITE)
@@ -97,9 +120,18 @@ def draw_text(scr, msg, pos, font, color) -> None:
 ### Main Stuff ###
 def main():
     load_words()
-    # word = random.choice(words)
-    word = "aback"
+    game_end = False
+    word_guessed = False
+    word = random.choice(words)
     word_slots = ["", "", "", "", "", ""]
+    word_slot_data = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
     current_word = ""
     current_attempt = 0
     error = ()
@@ -110,17 +142,12 @@ def main():
     
     # Event loop
     while True:
-        scr.fill(pygame.Color(0, 0, 0))
-        draw_text(scr, f"The word is \"{word}\"", (0, SCREEN_HEIGHT - (UI_FONT_SIZE * 2)), UI_FONT, COL_RIGHT_SPOT)
-
-        idk_anymore = (RECT_SIZE + RECT_PAD) * (SLOT_COUNT - 1)
-        draw_text(scr, f"Attempt {current_attempt + 1}/{SLOT_COUNT}", (idk_anymore, 0), UI_FONT, COL_WHITE)
-        
-        # Draw an error if there is one. This error tuple method just sucks.
-        if error:
-            draw_text(scr, error[1], (0, SCREEN_HEIGHT - UI_FONT_SIZE), UI_FONT, COL_ERROR)
+        scr.fill((0, 0, 0))
 
         for event in pygame.event.get():
+            if game_end:
+                break
+
             if event.type == pygame.QUIT:
                 pygame.quit()
             elif event.type == pygame.KEYDOWN:
@@ -132,16 +159,37 @@ def main():
                         print(current_word)
                         current_word = current_word[:-1]
                 elif event.key == pygame.K_RETURN:
-                    result = check_word(current_word)
+                    result = check_word(current_word, word, current_attempt, word_slot_data)
                     if result[0]:
                         word_slots[current_attempt] = current_word
+                        word_guessed = current_word == word
                         current_attempt += 1
                         current_word = ""
                         error = ()
+
+                        if word_guessed:
+                            game_end = True
+                            word_guessed = True
+                        elif current_attempt == SLOT_COUNT:
+                            game_end = True
                     else:
                         error = result
 
-        draw_ui(scr, word, word_slots, current_word, current_attempt)
+        # Drawing stuff
+        if game_end:
+            if not word_guessed:
+                draw_text(scr, f"The word was \"{word}\"", (0, SCREEN_HEIGHT - (UI_FONT_SIZE * 2)), UI_FONT, COL_ERROR)
+            else:
+                draw_text(scr, "You Won!", (0, SCREEN_HEIGHT - (UI_FONT_SIZE * 2)), UI_FONT, COL_RIGHT_SPOT)
+
+        idk_anymore = (RECT_SIZE + RECT_PAD) * (SLOT_COUNT - 1)
+        draw_text(scr, f"Attempt {current_attempt + 1}/{SLOT_COUNT}", (idk_anymore, 0), UI_FONT, COL_WHITE)
+        
+        # Draw an error if there is one. This error tuple method just sucks.
+        if error:
+            draw_text(scr, error[1], (0, SCREEN_HEIGHT - UI_FONT_SIZE), UI_FONT, COL_ERROR)
+
+        draw_ui(scr, word, word_slots, word_slot_data, current_word, current_attempt)
         pygame.display.flip()
 
 
